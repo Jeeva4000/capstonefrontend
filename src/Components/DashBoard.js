a9b86cad33929ea7d764858e45bc5457f8222f9a
Jeeva4000 <vjeeva@gmail.com>
2023-10-08 20:27:25 +0530

    cnange dasboard

--- src/Components/DashBoard.js
+++ src/Components/DashBoard.js
@@ -8,10 +8,10 @@ const Dashboard = () => {
   const [error, setError] = useState("");
   // const [loading, setLoading] = useState("");
   const navigate = useNavigate();
- 
+
   useEffect(() => {
     if (!localStorage.getItem("token")) {
-      navigate("/login", { replace: true });
+      navigate("/", { replace: true });
     }
     let token = localStorage.getItem("token");
     const fetchAllData = async () => {
@@ -22,46 +22,46 @@ const Dashboard = () => {
         },
       });
       const data = await res.json();
-     
+
 
       if (!data.data) {
         setError(data.message);
-      } 
+      }
       setCars(data.data)
       // console.log(cars)
     };
     fetchAllData();
-  }, [cars,navigate]);
+  }, [cars, navigate]);
 
   return (
     <Base>
-    {cars && (
-      <div className="container"> {/* Add a class to the container */}
-        {cars?.map((data, index) => (
-          <Paper elevation={6} key={data._id} className="paper"> {/* Add a class to the paper */}
-            <div className="img-container"> {/* Add a class to the image container */}
-              <img src={data.data.image} alt={data.image} />
-            </div>
-            <div className="details"> {/* Add a class to the details */}
-              <p>Make  : {data.data.make}</p>
-              <p>Model  : {data.data.model}</p>
-              <p>Price  : {data.data.price}</p>
-              <p>Color  : {data.data.color}</p>
-              <p>FuelType  :{data.data.fuelType}</p>
-             <p>Mileage  : {data.data.mileage}</p>
-              <p>Transmission  : {data.data.transmission}</p>
-              <p>Engine  : {data.data.engine}</p>
-              <p>Horsepower  : {data.data.horsepower}</p>
-              <p>Torque  : {data.data.torque}</p>
-              <p>Drivetrain  : {data.data.drivetrain}</p>
-            </div>
-          </Paper>
-        ))}
-      </div>
-    )}
-    {error ? <Typography color="error">{error}</Typography> : ""}
-  </Base>
-);
+      {cars && (
+        <div className="container"> {/* Add a class to the container */}
+          {cars?.map((data, index) => (
+            <Paper elevation={6} key={data._id} className="paper"> {/* Add a class to the paper */}
+              <div className="img-container"> {/* Add a class to the image container */}
+                <img src={data.data.image} alt={data.image} />
+              </div>
+              <div className="details"> {/* Add a class to the details */}
+                <p>Make  : {data.data.make}</p>
+                <p>Model  : {data.data.model}</p>
+                <p>Price  : {data.data.price}</p>
+                <p>Color  : {data.data.color}</p>
+                <p>FuelType  :{data.data.fuelType}</p>
+                <p>Mileage  : {data.data.mileage}</p>
+                <p>Transmission  : {data.data.transmission}</p>
+                <p>Engine  : {data.data.engine}</p>
+                <p>Horsepower  : {data.data.horsepower}</p>
+                <p>Torque  : {data.data.torque}</p>
+                <p>Drivetrain  : {data.data.drivetrain}</p>
+              </div>
+            </Paper>
+          ))}
+        </div>
+      )}
+      {error ? <Typography color="error">{error}</Typography> : ""}
+    </Base>
+  );
 };
 
 export default Dashboard;
