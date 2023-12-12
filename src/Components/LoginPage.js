c38ca58f092f3aabf6f693ec5416ba0eea1b4c65
Jeeva4000 <vjeeva@gmail.com>
2023-12-12 22:05:37 +0530

    navigate

--- src/Components/LoginPage.js
+++ src/Components/LoginPage.js
@@ -1,7 +1,80 @@
+// // import React, { useState } from "react"
+// // import Base from "../Base/Base"
+// // import { Button, TextField, Typography } from "@mui/material"
+// // import { Link, useNavigate } from "react-router-dom"
+// // const LoginPage = () => {
+// //     const navigate = useNavigate()
+// //     const [email, setEmail] = useState("");
+// //     const [password, setPassword] = useState("");
+// //     const [error, setError] = useState("");
+
+// //     const handleLogin = async () => {
+// //         const userDetails = {
+// //             email,
+// //             password
+// //         }
+// //         const response = await fetch(`https://capstonebackend-47kj.onrender.com/user/login`, {
+// //             method: "POST",
+// //             body: JSON.stringify(userDetails),
+// //             headers: {
+// //                 "Content-type": "application/json"
+
+// //             }
+// //         });
+
+// //         const data = await response.json()
+// //         if (data.token) {
+// //             setError("")
+// //             localStorage.setItem("token", data.token)
+// //             navigate("/")
+// //         } else {
+// //             setError(data.message)
+// //         }
+// //     }
+
+// //     return (
+// //         <Base>
+// //             <div className="box">
+// //                 <div className="form">
+// //                     <TextField label="email" variant="outlined" fullWidth sx={{ m: 1 }}
+// //                         placeholder="Enter the email"
+// //                         value={email}
+// //                         onChange={(e) => setEmail(e.target.value)}
+// //                         type="email"
+// //                     />
+// //                     <TextField label="password" variant="outlined" fullWidth sx={{ m: 1 }}
+// //                         placeholder="Enter the password"
+// //                         type="password"
+// //                         value={password}
+// //                         onChange={(e) => setPassword(e.target.value)}
+// //                     />
+// //                     <Button
+// //                         type="submit"
+// //                         variant="contained"
+// //                         onClick={handleLogin}
+// //                     >Login</Button>
+// //                     {error ?
+// //                         <Typography className="error" color={"danger"}>
+// //                             {error}
+// //                         </Typography> : ""}
+// //                 </div>
+// //             </div>
+// //         </Base>
+
+// //     )
+// // }
+
+// // export default LoginPage
+
+
+
 // import React, { useState } from "react"
 // import Base from "../Base/Base"
 // import { Button, TextField, Typography } from "@mui/material"
 // import { Link, useNavigate } from "react-router-dom"
+// import { toast } from "react-toastify"
+// import 'react-toastify/dist/ReactToastify.css';
+
 // const LoginPage = () => {
 //     const navigate = useNavigate()
 //     const [email, setEmail] = useState("");
@@ -13,29 +86,70 @@
 //             email,
 //             password
 //         }
-//         const response = await fetch(`https://capstonebackend-47kj.onrender.com/user/login`, {
-//             method: "POST",
-//             body: JSON.stringify(userDetails),
-//             headers: {
-//                 "Content-type": "application/json"
 
+//         try {
+//             const response = await fetch(`https://capstonebackend-47kj.onrender.com/user/login`, {
+//                 method: "POST",
+//                 body: JSON.stringify(userDetails),
+//                 headers: {
+//                     "Content-type": "application/json"
+//                 }
+//             });
+
+//             const data = await response.json();
+//             // console.log("API Response:", data);
+//             if (data.token) {
+//                 setError("");
+//                 localStorage.setItem("token", data.token);
+//                 navigate("/");
+//             } else {
+//                 setError(data.message);
+
+//                 if (data.message) {
+//                     toast.error("User not found. Please sign up.");
+//                 }
 //             }
-//         });
-
-//         const data = await response.json()
-//         if (data.token) {
-//             setError("")
-//             localStorage.setItem("token", data.token)
-//             navigate("/")
-//         } else {
-//             setError(data.message)
+//         } catch (error) {
+//             console.error("Error during login:", error);
+//             setError("An error occurred during login. Please try again.");
 //         }
-//     }
+//     };
 
 //     return (
+
+//         // <Base>
+
+//         //     <TextField label="email" variant="outlined" fullWidth sx={{ m: 1 }}
+//         //         placeholder="Enter the email"
+//         //         value={email}
+//         //         onChange={(e) => setEmail(e.target.value)}
+//         //         type="email"
+//         //     />
+//         //     <TextField label="password" variant="outlined" fullWidth sx={{ m: 1 }}
+//         //         placeholder="Enter the password"
+//         //         type="password"
+//         //         value={password}
+//         //         onChange={(e) => setPassword(e.target.value)}
+//         //     />
+
+
+
+//         //     <Button
+//         //         className="login-button"
+//         //         type="submit"
+//         //         variant="contained"
+//         //         onClick={handleLogin}
+//         //     >Login</Button>
+//         //     {error ?
+//         //         <Typography color={"danger"}>
+//         //             {error}
+//         //         </Typography> : ""}
+
+//         // </Base >
 //         <Base>
 //             <div className="box">
 //                 <div className="form">
+//                     <p className="lo">LOGIN</p>
 //                     <TextField label="email" variant="outlined" fullWidth sx={{ m: 1 }}
 //                         placeholder="Enter the email"
 //                         value={email}
@@ -57,6 +171,15 @@
 //                         <Typography className="error" color={"danger"}>
 //                             {error}
 //                         </Typography> : ""}
+//                     <div className="lt">
+//                         <span>Don't have an account? </span>
+//                         <Link to="/signup">Sign Up</Link>
+//                     </div>
+//                     <div>
+//                         <h2>For Testing:</h2>
+//                         <h3>Email: vjeeva@gmail.com   </h3>
+//                         <h3>Password: 12345  </h3>
+//                     </div>
 //                 </div>
 //             </div>
 //         </Base>
@@ -64,19 +187,20 @@
 //     )
 // }
 
+
+
+
 // export default LoginPage
 
-
-
-import React, { useState } from "react"
-import Base from "../Base/Base"
-import { Button, TextField, Typography } from "@mui/material"
-import { Link, useNavigate } from "react-router-dom"
-import { toast } from "react-toastify"
+import React, { useState } from "react";
+import Base from "../Base/Base";
+import { Button, TextField, Typography } from "@mui/material";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
 const LoginPage = () => {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
@@ -85,7 +209,7 @@ const LoginPage = () => {
         const userDetails = {
             email,
             password
-        }
+        };
 
         try {
             const response = await fetch(`https://capstonebackend-47kj.onrender.com/user/login`, {
@@ -97,55 +221,27 @@ const LoginPage = () => {
             });
 
             const data = await response.json();
-            // console.log("API Response:", data);
             if (data.token) {
                 setError("");
                 localStorage.setItem("token", data.token);
                 navigate("/");
+                toast.success("Login successful!");
             } else {
                 setError(data.message);
-
-                if (data.message) {
-                    toast.error("User not found. Please sign up.");
-                }
+                toast.error(data.message || "An error occurred during login. Please try again.");
             }
         } catch (error) {
             console.error("Error during login:", error);
             setError("An error occurred during login. Please try again.");
+            toast.error("An error occurred during login. Please try again.");
         }
     };
+    const handleDemoLogin = () => {
 
+        setEmail("vjeeva@gmail.com");
+        setPassword("12345");
+    }
     return (
-
-        // <Base>
-
-        //     <TextField label="email" variant="outlined" fullWidth sx={{ m: 1 }}
-        //         placeholder="Enter the email"
-        //         value={email}
-        //         onChange={(e) => setEmail(e.target.value)}
-        //         type="email"
-        //     />
-        //     <TextField label="password" variant="outlined" fullWidth sx={{ m: 1 }}
-        //         placeholder="Enter the password"
-        //         type="password"
-        //         value={password}
-        //         onChange={(e) => setPassword(e.target.value)}
-        //     />
-
-
-
-        //     <Button
-        //         className="login-button"
-        //         type="submit"
-        //         variant="contained"
-        //         onClick={handleLogin}
-        //     >Login</Button>
-        //     {error ?
-        //         <Typography color={"danger"}>
-        //             {error}
-        //         </Typography> : ""}
-
-        // </Base >
         <Base>
             <div className="box">
                 <div className="form">
@@ -167,27 +263,33 @@ const LoginPage = () => {
                         variant="contained"
                         onClick={handleLogin}
                     >Login</Button>
-                    {error ?
+                    {error &&
                         <Typography className="error" color={"danger"}>
                             {error}
-                        </Typography> : ""}
+                        </Typography>
+                    }
+                    <Button
+                        className="demo"
+                        variant="contained"
+                        onClick={handleDemoLogin}
+                        sx={{ mt: 2 }}
+                    >
+                        Demo Credentials
+                    </Button>
                     <div className="lt">
                         <span>Don't have an account? </span>
                         <Link to="/signup">Sign Up</Link>
                     </div>
-                    <div>
+                    {/* <div>
                         <h2>For Testing:</h2>
                         <h3>Email: vjeeva@gmail.com   </h3>
                         <h3>Password: 12345  </h3>
-                    </div>
+                    </div> */}
+
                 </div>
             </div>
         </Base>
+    );
+};
 
-    )
-}
-
-
-
-
-export default LoginPage
+export default LoginPage;
--- src/Components/SignupPage.js
+++ src/Components/SignupPage.js
@@ -1,30 +1,116 @@
+// // import React, { useState } from 'react';
+// // import Base from '../Base/Base.js';
+// // import { useNavigate } from 'react-router-dom';
+
+// // const SignupPage = () => {
+// //   const [email, setEmail] = useState('');
+// //   const [password, setPassword] = useState('');
+// //   const [confirmPassword, setConfirmPassword] = useState('');
+// //   const [error, setError] = useState('');
+// //   const [loading, setLoading] = useState(false); // Added loading state
+// //   const navigate = useNavigate();
+
+// //   const handleSignup = async () => {
+// //     if (password !== confirmPassword) {
+// //       setError('Passwords do not match');
+// //       return;
+// //     }
+
+// //     const userDetail = {
+// //       email,
+// //       password,
+// //       confirmPassword,
+// //     };
+
+// //     setLoading(true);
+
+// //     try {
+// //       const response = await fetch('https://capstonebackend-47kj.onrender.com/user/signup', {
+// //         method: 'POST',
+// //         body: JSON.stringify(userDetail),
+// //         headers: {
+// //           'Content-Type': 'application/json',
+// //         },
+// //       });
+
+// //       const data = await response.json();
+
+// //       if (data.token) {
+// //         setError('');
+// //         localStorage.setItem('token', data.token);
+// //         // Use await to ensure navigation after successful response
+// //         await navigate('/login');
+// //       } else {
+// //         setError(data.message);
+// //       }
+// //     } catch (error) {
+// //       console.error(error);
+// //       setError('An unexpected error occurred. Please try again later.');
+// //     } finally {
+// //       setLoading(false); // Reset loading state
+// //     }
+// //   };
+
+// //   return (
+// //     <Base>
+// //       <div className="main">
+// //         <div className="signup-form">
+// //           <input
+// //             type="email"
+// //             placeholder="Email"
+// //             value={email}
+// //             onChange={(e) => setEmail(e.target.value)}
+// //           />
+// //           <input
+// //             type="password"
+// //             placeholder="Password"
+// //             value={password}
+// //             onChange={(e) => setPassword(e.target.value)}
+// //           />
+// //           <input
+// //             type="password"
+// //             placeholder="Confirm Password"
+// //             value={confirmPassword}
+// //             onChange={(e) => setConfirmPassword(e.target.value)}
+// //           />
+// //           <button onClick={handleSignup} disabled={loading}>
+// //             {loading ? 'Signing up...' : 'Sign Up'}
+// //           </button>
+// //           {error && <p className="error-message">{error}</p>}
+// //         </div>
+// //       </div>
+// //     </Base>
+// //   );
+// // };
+
+// // export default SignupPage;
+
+
+
+
 // import React, { useState } from 'react';
 // import Base from '../Base/Base.js';
-// import { useNavigate } from 'react-router-dom';
+// import { Link, useNavigate } from 'react-router-dom';
 
 // const SignupPage = () => {
+
 //   const [email, setEmail] = useState('');
 //   const [password, setPassword] = useState('');
 //   const [confirmPassword, setConfirmPassword] = useState('');
 //   const [error, setError] = useState('');
-//   const [loading, setLoading] = useState(false); // Added loading state
 //   const navigate = useNavigate();
 
 //   const handleSignup = async () => {
-//     if (password !== confirmPassword) {
-//       setError('Passwords do not match');
-//       return;
-//     }
 
 //     const userDetail = {
 //       email,
 //       password,
 //       confirmPassword,
 //     };
-
-//     setLoading(true);
+//     console.log(userDetail)
 
 //     try {
+
 //       const response = await fetch('https://capstonebackend-47kj.onrender.com/user/signup', {
 //         method: 'POST',
 //         body: JSON.stringify(userDetail),
@@ -34,20 +120,19 @@
 //       });
 
 //       const data = await response.json();
+//       console.log(data)
 
 //       if (data.token) {
 //         setError('');
 //         localStorage.setItem('token', data.token);
-//         // Use await to ensure navigation after successful response
-//         await navigate('/login');
+//         // navigate('/login');
+//         window.location.href = "/login";
 //       } else {
 //         setError(data.message);
 //       }
 //     } catch (error) {
-//       console.error(error);
+//       console.error(error)
 //       setError('An unexpected error occurred. Please try again later.');
-//     } finally {
-//       setLoading(false); // Reset loading state
 //     }
 //   };
 
@@ -55,6 +140,8 @@
 //     <Base>
 //       <div className="main">
 //         <div className="signup-form">
+//           <p className='sign'>SIGNUP</p>
+
 //           <input
 //             type="email"
 //             placeholder="Email"
@@ -73,10 +160,13 @@
 //             value={confirmPassword}
 //             onChange={(e) => setConfirmPassword(e.target.value)}
 //           />
-//           <button onClick={handleSignup} disabled={loading}>
-//             {loading ? 'Signing up...' : 'Sign Up'}
-//           </button>
+//           <button onClick={handleSignup}>Sign Up</button>
 //           {error && <p className="error-message">{error}</p>}
+
+//           <div className='log'>
+//             <span>Do you have an account? </span>
+//             <Link to="/login">Login</Link>
+//           </div>
 //         </div>
 //       </div>
 //     </Base>
@@ -87,30 +177,26 @@
 
 
 
-
 import React, { useState } from 'react';
 import Base from '../Base/Base.js';
 import { Link, useNavigate } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const SignupPage = () => {
-
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
-  const [error, setError] = useState('');
   const navigate = useNavigate();
 
   const handleSignup = async () => {
-
     const userDetail = {
       email,
       password,
       confirmPassword,
     };
-    console.log(userDetail)
 
     try {
-
       const response = await fetch('https://capstonebackend-47kj.onrender.com/user/signup', {
         method: 'POST',
         body: JSON.stringify(userDetail),
@@ -120,18 +206,22 @@ const SignupPage = () => {
       });
 
       const data = await response.json();
-      console.log(data)
 
       if (data.token) {
-        setError('');
+        // Signup successful
         localStorage.setItem('token', data.token);
         navigate('/login');
+        toast.success('Signup successful! Please log in.');
+      } else if (data.error === 'Email already exists') {
+        // Email already exists
+        toast.error('Email already exists. Please use a different email.');
       } else {
-        setError(data.message);
+        // Other error
+        toast.error('An unexpected error occurred. Please try again later.');
       }
     } catch (error) {
-      console.error(error)
-      setError('An unexpected error occurred. Please try again later.');
+      console.error(error);
+      toast.error('An unexpected error occurred. Please try again later.');
     }
   };
 
@@ -160,12 +250,13 @@ const SignupPage = () => {
             onChange={(e) => setConfirmPassword(e.target.value)}
           />
           <button onClick={handleSignup}>Sign Up</button>
-          {error && <p className="error-message">{error}</p>}
 
           <div className='log'>
             <span>Do you have an account? </span>
             <Link to="/login">Login</Link>
           </div>
+
+          <ToastContainer position="top-right" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
         </div>
       </div>
     </Base>
@@ -173,3 +264,4 @@ const SignupPage = () => {
 };
 
 export default SignupPage;
+
